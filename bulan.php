320cb7f480aec739ddd2c4df7bb2f17875d6bf7b
shiin12 <afwanayasyin12345@gmail.com>
2025-03-05 22:06:12 +0700

    tugas 2 rev

--- bulan.php
+++ bulan.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Afwan Ayasyin</title>
+    <title>bulan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -36,14 +36,11 @@
             </div>
         </div>
     </nav>
-    masukan bulan : <br> <input type="number" name="bulan">
-    <br><input type="submit"> <input type="reset"><br>
-    <?php
-    $bulan = $_POST["bulan"];
-    $bulan = date("F");
-    echo "$bulan <br>";
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <form action="outputbulan.php" method="post">
+        masukan bulan dalam angka : <br> <input type="number" name="bulan">
+        <br><input type="submit"> <input type="reset"><br>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 <div>
     <footer>
--- outputbulan.php
+++ outputbulan.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>segitiga</title>
+    <title>bulan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -36,11 +36,36 @@
             </div>
         </div>
     </nav>
-    <?php $alas = $_POST["alas"];
-    $tinggi = $_POST["tinggi"];
-    $hasil = $alas * $tinggi / 2;
-    echo "luas segitiga = $hasil";
-    echo "<br>";
+    <?php
+    $bulan = $_POST["bulan"];
+
+    if ($bulan == 1) {
+        echo "Bulan Januari";
+    } elseif ($bulan == 2) {
+        echo "Bulan Februari";
+    } elseif ($bulan == 3) {
+        echo "Bulan Maret";
+    } elseif ($bulan == 4) {
+        echo "Bulan April";
+    } elseif ($bulan == 5) {
+        echo "Bulan Mei";
+    } elseif ($bulan == 6) {
+        echo "Bulan Juni";
+    } elseif ($bulan == 7) {
+        echo "Bulan Juli";
+    } elseif ($bulan == 8) {
+        echo "Bulan Agustus";
+    } elseif ($bulan == 9) {
+        echo "Bulan September";
+    } elseif ($bulan == 10) {
+        echo "Bulan Oktober";
+    } elseif ($bulan == 11) {
+        echo "Bulan November";
+    } elseif ($bulan == 12) {
+        echo "Bulan Desember";
+    } else {
+        echo "tidak tersedia";
+    }
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
